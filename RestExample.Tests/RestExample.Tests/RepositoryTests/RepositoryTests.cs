8dd3e10f80c9e8b8b08ad1728a650175ec16ec1e
Ben Tse <benjamin.l.tse@gmail.com>
2019-05-16 22:53:33 +1000

    Implement repo create, read and delete

---
+++ RestExample.Tests/RestExample.Tests/RepositoryTests/RepositoryTests.cs
@@ -0,0 +1,116 @@
+﻿using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using RestExample.Contracts;
+using RestExample.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestExample.Tests.RepositoryTests
+{
+    public class RepositoryTests
+    {
+        [Test]
+        public async Task NewlyCreatedProduct_CanbeRead()
+        {
+            // arrange
+            var repo = CreateRepository();
+            var product = Builder<ProductEntity>.CreateNew()
+                .With(entity => entity.Description = "Super product")
+                .And(entity => entity.Brand = "Reliable brand")
+                .And(entity => entity.Model = "Top of the line")
+                .And(entity => entity.Id = "ignorableId")
+                .Build();
+
+            var id = await repo.CreateProduct(product);
+
+            // act
+            var readProduct = await repo.GetProduct(id);
+
+            // assert
+            id.Should().NotBe("ignorableId");
+            readProduct.Description.Should().Be("Super product");
+            readProduct.Brand.Should().Be("Reliable brand");
+            readProduct.Model.Should().Be("Top of the line");
+        }
+
+        [Test]
+        public async Task NewlyCreatedProducts_CanbeRead()
+        {
+            // arrange
+            var repo = CreateRepository();
+            var products = Builder<ProductEntity>.CreateListOfSize(2)
+                .Build();
+
+            var productIds = new List<string>();
+            foreach (var product in products)
+            {
+                productIds.Add(await repo.CreateProduct(product));
+            }
+
+            // act
+            var readProducts = await repo.GetAllProducts();
+
+            // assert
+            readProducts.Should().HaveCount(2);
+            readProducts.Should().Contain(product => product.Id == productIds.First());
+            readProducts.Should().Contain(product => product.Id == productIds.Skip(1).First());
+        }
+
+        [Test]
+        public async Task CreatedProduct_CanBeDeleted()
+        {
+            // arrange
+            var repo = CreateRepository();
+            var product = Builder<ProductEntity>.CreateNew()
+                .With(entity => entity.Description = "Super product")
+                .And(entity => entity.Brand = "Reliable brand")
+                .And(entity => entity.Model = "Top of the line")
+                .And(entity => entity.Id = "ignorableId")
+                .Build();
+
+            var id = await repo.CreateProduct(product);
+
+            // act
+            await repo.DeleteProduct(id);
+            var repoProduct = await repo.GetProduct(id);
+
+            // assert
+            repoProduct.Should().BeNull();
+        }
+
+        [Test]
+        public async Task GettingAnUnknownProduct_ReturnsNull()
+        {
+            // arrange
+            var repo = CreateRepository();
+
+            // act
+            var repoProduct = await repo.GetProduct("unknown product");
+
+            // assert
+            repoProduct.Should().BeNull();
+        }
+
+        [Test]
+        public async Task DeletingUnknownProduct_ThrowsException()
+        {
+            // arrange
+            var repo = CreateRepository();
+
+            // act
+            Func<Task> fn = async () => await repo.DeleteProduct("unknown product");
+
+            // assert
+            fn.Should().Throw<KeyNotFoundException>();
+        }
+
+
+        private IProductRepository CreateRepository()
+        {
+            return new ProductRepository();
+        }
+    }
+}
---
+++ RestExample/RestExample/Contracts/ProductRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestExample.Model;
+
+namespace RestExample.Contracts
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ConcurrentDictionary<string, ProductEntity> _dictionary = new ConcurrentDictionary<string, ProductEntity>();
+
+        public Task<string> CreateProduct(ProductEntity product)
+        {
+            var id = Guid.NewGuid().ToString();
+            product.Id = id;
+
+            _dictionary.TryAdd(id, product);
+
+            return Task.FromResult(id);
+        }
+
+        public Task DeleteProduct(string id)
+        {
+            if (!_dictionary.TryRemove(id, out ProductEntity deletedProduct))
+            {
+                throw new KeyNotFoundException($"Could not delete product id {id} - product not found");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task<List<ProductEntity>> GetAllProducts()
+        {
+            var repositorySnapshot = _dictionary.ToArray();
+
+            var productList = repositorySnapshot.Select(kvp => kvp.Value).ToList();
+
+            return Task.FromResult(productList);
+        }
+
+        public Task<ProductEntity> GetProduct(string id)
+        {
+            ProductEntity product = null;
+            _dictionary.TryGetValue(id, out product);
+
+            return Task.FromResult(product);
+        }
+
+        public Task UpdateProduct(ProductEntity product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
